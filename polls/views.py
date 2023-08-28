9f3aba25cd3901d9114b7cefa883dbd60d5fcb7c
sosokker <sirin_29@hotmail.co.th>
2023-08-28 23:04:08 +0700

    Add docstring to classes and methods and clean the code.

--- polls/views.py
+++ polls/views.py
@@ -9,6 +9,10 @@ from .models import Choice, Question
 
 
 class HomeView(TemplateView):
+    """
+    Provide a view for Home page(first page).
+    """
+
     template_name = 'polls/home.html'
 
     def get_context_data(self, **kwargs):
@@ -19,6 +23,10 @@ class HomeView(TemplateView):
 
 
 class IndexView(generic.ListView):
+    """
+    Provide a view for Index page that list all polls.
+    """
+
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
@@ -33,6 +41,11 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
+    """
+    Provide a view for detail page, a detail for each poll contain poll question
+    and poll choices.
+    """
+
     model = Question
     template_name = "polls/detail.html"
 
@@ -42,12 +55,22 @@ class DetailView(generic.DetailView):
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+
 class ResultsView(generic.DetailView):
+    """
+    Provide a view for result page that show up when user submit on of the choices.
+    """
+
     model = Question
     template_name = "polls/results.html"
 
 
 def vote(request, question_id):
+    """
+    A function that update the database. Add vote count to choice that user vote
+    in specific question_id.
+    """
+
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
@@ -64,3 +87,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
