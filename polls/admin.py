c54492c4188dea88bbf58f4e773f47f47e1a2d3b
sosokker <sirin_29@hotmail.co.th>
2023-09-07 22:43:54 +0700

    Add end_date customizer to admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -12,8 +12,9 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+        ("End date", {"fields": ["end_date"], "classes": ["collapse"]}),
     ]
-    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_display = ["question_text", "pub_date", "end_date", "was_published_recently"]
     inlines = [ChoiceInline]
     list_filter = ["pub_date"]
     search_fields = ["question_text"]
