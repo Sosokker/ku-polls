f33dbdf1d56f184d8185d175c2c8958c5097a122
sosokker <sirin_29@hotmail.co.th>
2023-09-09 00:54:54 +0700

    Delete update pub_date field in admin

--- polls/admin.py
+++ polls/admin.py
@@ -11,7 +11,6 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
         ("End date", {"fields": ["end_date"], "classes": ["collapse"]}),
     ]
     list_display = ["question_text", "pub_date", "end_date", "was_published_recently"]
