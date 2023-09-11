50874dbf40b1f619845e42d22a74720884d513af
sosokker <sirin_29@hotmail.co.th>
2023-09-12 02:47:21 +0700

    Fix query in Index / Check User Vote/ Refine Model
    - Use Model from domain model
    - Change Vote Query because I change model
    - Write some properties such as participants in Question

--- polls/static/polls/js/detail.js
+++ polls/static/polls/js/detail.js
@@ -1,38 +1,82 @@
 const toggleChoice = (button, choiceId) => {
-    const choiceInput = document.querySelector(`input[name="choice"][value="${choiceId}"]`);
+  const choiceInput = document.querySelector(`input[name="choice"][value="${choiceId}"]`);
 
+  const selectedChoice2 = document.getElementById("selected-choice-1");
+
+  if (selectedChoice2 !== null) {
     if (choiceInput) {
-        // already selected -> unselect it
-        if (choiceInput.checked) {
-            choiceInput.checked = false;
-            button.classList.remove('bg-green-500', 'border-solid', 'border-2', 'border-black', 'hover:bg-green-600');
-            button.classList.add('bg-white', 'border-solid', 'border-2', 'border-black', 'hover:bg-white');
-            // Clear display
-            document.getElementById('selected-choice').textContent = 'Please Select a Choice😊';
-        } else {
-            // Unselect all choices
-            document.querySelectorAll('input[name="choice"]').forEach((choice) => {
-                choice.checked = false;
-            });
+      // already selected -> unselect it
+      if (choiceInput.checked) {
+        choiceInput.checked = false;
+        button.classList.remove("bg-green-500", "border-solid", "border-2", "border-black", "hover:bg-green-600");
+        button.classList.add("bg-white", "border-solid", "border-2", "border-black", "hover:bg-white");
+        // Clear display
+        document.getElementById("selected-choice-1").textContent = "You have been voted😊 Anyway, you can change the choice anytime before end date";
+      } else {
+        // Unselect all choices
+        document.querySelectorAll('input[name="choice"]').forEach(choice => {
+          choice.checked = false;
+        });
 
-            // Select the clicked choice
-            choiceInput.checked = true;
+        // Select the clicked choice
+        choiceInput.checked = true;
 
-            // Reset the style of all choice buttons
-            document.querySelectorAll('.choice-button').forEach((btn) => {
-                btn.classList.remove('bg-green-500', 'border-solid', 'border-2', 'border-black', 'hover:bg-green-600');
-                btn.classList.add('bg-white', 'border-solid', 'border-2', 'border-black', 'hover:bg-white');
-            });
+        // Reset the style of all choice buttons
+        document.querySelectorAll(".choice-button").forEach(btn => {
+          btn.classList.remove("bg-green-500", "border-solid", "border-2", "border-black", "hover:bg-green-600");
+          btn.classList.add("bg-white", "border-solid", "border-2", "border-black", "hover:bg-white");
+        });
 
-            button.classList.remove('bg-white', 'border-solid', 'border-2', 'border-black', 'hover:bg-white');
-            button.classList.add('bg-green-500', 'border-solid', 'border-2', 'border-black', 'hover:bg-green-600');
+        button.classList.remove("bg-white", "border-solid", "border-2", "border-black", "hover:bg-white");
+        button.classList.add("bg-green-500", "border-solid", "border-2", "border-black", "hover:bg-green-600");
 
-            const choiceText = button.textContent.trim();
-            document.getElementById('selected-choice').textContent = `You select: ${choiceText}`;
-        }
-
-        // Enable the "Vote" button -> if select
-        const voteButton = document.getElementById('vote-button');
-        voteButton.disabled = !document.querySelector('input[name="choice"]:checked');
+        const choiceText = button.textContent.trim();
+        document.getElementById("selected-choice-1").textContent = `You select: ${choiceText}`;
+      }
     }
+    // Enable the "Vote" button -> if select
+    const voteButton = document.getElementById("vote-button");
+    voteButton.disabled = !document.querySelector('input[name="choice"]:checked');
+  } else {
+    if (choiceInput) {
+      // already selected -> unselect it
+      if (choiceInput.checked) {
+        choiceInput.checked = false;
+        button.classList.remove("bg-green-500", "border-solid", "border-2", "border-black", "hover:bg-green-600");
+        button.classList.add("bg-white", "border-solid", "border-2", "border-black", "hover:bg-white");
+        // Clear display
+        document.getElementById("selected-choice-2").textContent = "Please Select a Choice😊";
+      } else {
+        // Unselect all choices
+        document.querySelectorAll('input[name="choice"]').forEach(choice => {
+          choice.checked = false;
+        });
+
+        // Select the clicked choice
+        choiceInput.checked = true;
+
+        // Reset the style of all choice buttons
+        document.querySelectorAll(".choice-button").forEach(btn => {
+          btn.classList.remove("bg-green-500", "border-solid", "border-2", "border-black", "hover:bg-green-600");
+          btn.classList.add("bg-white", "border-solid", "border-2", "border-black", "hover:bg-white");
+        });
+
+        button.classList.remove("bg-white", "border-solid", "border-2", "border-black", "hover:bg-white");
+        button.classList.add("bg-green-500", "border-solid", "border-2", "border-black", "hover:bg-green-600");
+
+        const choiceText = button.textContent.trim();
+        document.getElementById("selected-choice-2").textContent = `You select: ${choiceText}`;
+      }
+    }
+    // Enable the "Vote" button -> if select
+    const voteButton = document.getElementById("vote-button");
+    voteButton.disabled = !document.querySelector('input[name="choice"]:checked');
+  }
 };
+
+function confirmChangeVote(choiceId) {
+  const confirmation = confirm("Are you sure you want to change your vote?");
+  if (confirmation) {
+    window.location.href = `{% url 'polls:vote' question.id %}${choiceId}`;
+  }
+}
--- polls/views.py
+++ polls/views.py
@@ -7,9 +7,10 @@ from django.urls import reverse_lazy, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from .forms import SignUpForm
-from .models import Choice, Question
+from .models import Choice, Question, Vote
 
 
 class IndexView(generic.ListView):
@@ -19,10 +20,13 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
-            "-pub_date"
-        )[:5]
+            """
+            Return the last published questions that is published and haven't ended yet.
+            """
+            now = timezone.now()
+            return Question.objects.filter(
+                Q(pub_date__lte=now) & (Q(end_date__gte=now) | Q(end_date=None))
+            ).order_by("-pub_date")
 
 
 class DetailView(LoginRequiredMixin, generic.DetailView):
@@ -52,7 +56,21 @@ class DetailView(LoginRequiredMixin, generic.DetailView):
         context["end_date"] = question.end_date
         context["up_vote_count"] = question.up_vote_count
         context["down_vote_count"] = question.down_vote_count
-        context["participant_count"] = question.participant_count
+
+        user = self.request.user
+        selected_choice = None
+        has_voted = False
+
+        if user.is_authenticated:
+            try:
+                vote = question.vote_set.get(user=user)
+                selected_choice = vote.choice
+                has_voted = True
+            except Vote.DoesNotExist:
+                pass
+
+        context["selected_choice"] = selected_choice
+        context["has_voted"] = has_voted
 
         return context
 
@@ -60,11 +78,6 @@ class DetailView(LoginRequiredMixin, generic.DetailView):
 class ResultsView(LoginRequiredMixin, generic.DetailView):
     model = Question
     template_name = "polls/results.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["question"] = self.object
-        return context
 
     def render_to_response(self, context, **response_kwargs):
         return render(self.request, self.template_name, context)
@@ -87,12 +100,23 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         messages.error(request, "You didn't select a choice.")
         return render(request, "polls/detail.html", {"question": question})
+
     else:
-        if request.method == "POST" and "vote-button" in request.POST:
-            selected_choice.votes += 1
-            selected_choice.save()
-            messages.success(request, "Your vote was recorded successfully🥳")
-            return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        if question.can_vote():
+            if request.method == "POST" and "vote-button" in request.POST:
+                if Vote.objects.filter(user=request.user, question=question).exists():
+                    old_vote = question.vote_set.get(user=request.user)
+                    old_vote.choice = selected_choice
+                    old_vote.save()
+
+                    messages.success(request, "You vote successfully🥳")
+                    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+                else:
+                    Vote.objects.create(choice=selected_choice, user=request.user, question=question).save()
+            else:
+                messages.error(request, "You cannot vote by typing the URL.")
+                return render(request, "polls/detail.html", {"question": question})
         else:
-            messages.error(request, "You cannot vote by typing the URL.")
-            return render(request, "polls/detail.html", {"question": question})
+            messages.error(request, "You can not vote on this question.")
+            return HttpResponseRedirect(reverse("polls:index"))
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
