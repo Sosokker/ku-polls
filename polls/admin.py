e733fac7aa64b8c8700086729d785f388a08c55c
sosokker <sirin_29@hotmail.co.th>
2023-09-12 02:58:55 +0700

    Add success message when vote again/ delete participate in admin

--- polls/admin.py
+++ polls/admin.py
@@ -14,7 +14,6 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Published date", {"fields": ["pub_date"], "classes": ["collapse"]}),
         ("End date", {"fields": ["end_date"], "classes": ["collapse"]}),
         ("Sentiment Vote count", {"fields": ["up_vote_count", "down_vote_count"]}),
-        ("Participant count", {"fields": ["participant_count"]}),
     ]
     list_display = ["question_text", "pub_date", "end_date", "was_published_recently", "can_vote"]
     inlines = [ChoiceInline]
--- polls/views.py
+++ polls/views.py
@@ -112,7 +112,9 @@ def vote(request, question_id):
                     messages.success(request, "You vote successfully🥳")
                     return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
                 else:
+                    messages.success(request, "You vote successfully🥳")
                     Vote.objects.create(choice=selected_choice, user=request.user, question=question).save()
+                    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
             else:
                 messages.error(request, "You cannot vote by typing the URL.")
                 return render(request, "polls/detail.html", {"question": question})
